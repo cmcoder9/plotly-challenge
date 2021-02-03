590d2595a4253be482ae59574635e4c0da53b23b
cmcoder9 <cmcoder@gmail.com>
2021-02-02 16:43:05 -0800

    bar graph update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,41 +1,45 @@
+var data = {};
 
 // Use the D3 library to read in samples.json
-d3.json("samples.json").then((samples) => {
-    //  Create the Traces
+d3.json('samples.json').then(function(data) {
+  console.log(data);
+
+// Use D3 to select the dropdown menu
+var dropdownMenu = d3.select('#selDataset');
+
+  //  Create the Traces
     var trace1 = {
-      x: samples.sample_values,
-      y: samples.otu_ids,
-      type: "bar",
+      x: data.samples[0].sample_values.slice(0,10),
+      y: data.samples[0].otu_ids,
+      hover_data: otu_labels,
       orientation: 'h',
+      type: 'bar',
+      width: 130 
     };
   
     // Create the data array for the plot
-    var data = [trace1];
+    var bar_data = [trace1];
   
     // Define the plot layout
-    var layout = {
-        title: 'Top 10 OTUs',
+    var bar_layout = {
+        title: 'OTUs Occurance',
         font:{
           family: 'Raleway, sans-serif'
         },
         showlegend: false,
-        xaxis: {
+        xaxis: { 
+          title: "OTU ID",
           tickangle: -45
         },
         yaxis: {
+          title: "Frequency",
           zeroline: false,
           gridwidth: 2
         },
         bargap :0.05
       };
-      
-    // Use D3 to select the dropdown menu
-    var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
-
-
-      Plotly.newPlot('bar', data, layout);
+    
+      Plotly.newPlot('bar', bar_data, bar_layout);
+});
 
     
-};
